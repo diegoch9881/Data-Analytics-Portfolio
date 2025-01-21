a7ec55846b117d53443280c984436535ee1e4775
Diego Chávez <diegochavez011998@gmail.com>
2025-01-21 13:42:19 -0500

    Update Total_Sales_by_Region.sql

--- SQL/Total_Sales_by_Region.sql
+++ SQL/Total_Sales_by_Region.sql
@@ -7,3 +7,11 @@ SELECT Region,
 FROM apple_sales_2024
 GROUP BY Region
 ORDER BY Total_iPhone_Sales DESC;
+
+-'The GROUP BY clause ensures that the aggregation is done at the regional level'
+-'The ORDER BY clause orders the results by the total iPhone sales in descending       
+order, highlighting the regions with the highest iPhone sales'
+
+-'The results of this query give a clear picture of how Apple’s products are       
+performing across regions in 2024, helping to identify regions where iPhone       
+sales are the strongest.'
